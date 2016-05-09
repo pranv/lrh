57db7666ae351833869d07843e68c46c9237e31d
Pranav Shyam <pranavshyam13@gmail.com>
2016-05-09 15:01:59 +0530

    added base.py - base layer abstract class

---
+++ layers/base.py
@@ -0,0 +1,27 @@
+class Layer(object):
+	def __init__(self):
+		pass
+
+	def forward(self):
+		pass
+
+	def backward(self):
+		pass
+
+	def get_params(self):
+		pass
+
+	def set_params(self):
+		pass
+
+	def get_grads(self):
+		pass
+
+	def clear_grads(self):
+		pass
+
+	def forget(self):
+		pass
+
+	def remember(self):
+		pass
